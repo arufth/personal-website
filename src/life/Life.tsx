654ab3b2ae25bb76b520bdb45772520c4c24b673
carlitosdummy <carlos256800@gmail.com>
2024-09-07 16:53:55 -0600

    add the basic structure for the life tab

--- src/life/Life.tsx
+++ src/life/Life.tsx
@@ -1,10 +1,24 @@
-import { NavBar } from '../components'
+import { Header } from '../components'
+
+import './Life.css'
+import photos from './Life.json'
 
 export const Life: React.FC = () => {
   return (
     <>
-      <NavBar />
-      Life
+      <main className='main-life'>
+        <Header title="POV: you're Aru" subtitle='I used to be a ' />
+        <ul>
+          {
+            photos.map(photo => (
+              <li key={photo.id}>
+                <img src={photo.path} alt={photo.title} />
+                <h3>{photo.title}</h3>
+              </li>
+            ))
+          }
+        </ul>
+      </main>
     </>
   )
 }
